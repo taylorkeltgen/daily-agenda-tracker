868a17f2c41b3423a0be5f6d625c2ade7296ec56
taylorkeltgen <taylorkeltgen@gmail.com>
2021-10-24 18:06:39 -0500

    add buttons

--- asset/js/script.js
+++ asset/js/script.js
@@ -9,88 +9,145 @@ var twoEl = document.querySelector('#twoText');
 var threeEl = document.querySelector('#threeText');
 var fourEl = document.querySelector('#fourText');
 var fiveEl = document.querySelector('#fiveText');
+// Save Buttons
+var buttonNineEl = document.querySelector("#btnNine");
+var buttonTenEl = document.querySelector("#btnTen");
+var buttonElevenEl = document.querySelector("#btnEleven");
+var buttonTwelveEl = document.querySelector("#btnTwelve");
+var buttonOneEl = document.querySelector("#btnOne");
+var buttonTwoEl = document.querySelector("#btnTwo");
+var buttonThreeEl = document.querySelector("#btnThree");
+var buttonFourEl = document.querySelector("#btnFour");
+var buttonFiveEl = document.querySelector("#btnFive");
 
 var today = moment().format('dddd, MMMM Do');
 
+var agenda = [
+    {
+        time: 9,
+        content: ""
+    },
+    {
+        time: 10,
+        content: ""
+    },
+    {
+        time: 11,
+        content: ""
+    },
+    {
+        time: 12,
+        content: ""
+    },
+    {
+        time: 1,
+        content: ""
+    },
+    {
+        time: 2,
+        content: ""
+    },
+    {
+        time: 3,
+        content: ""
+    },
+    {
+        time: 4,
+        content: ""
+    },
+    {
+        time: 5,
+        content: ""
+    },
+]
 
 // Set Current Date in Jumbotron
 currentDayEl.textContent = today;
 
 var getCurrentTime = function(){
     var currentHour = moment().hour();
-    console.log('Current hour: ' + currentHour);
-    // 9AM Time Block
-    if(9 < currentHour){
-        nineEl.classList = 'past';
-    } else if (9 > currentHour){
-        nineEl.classList = 'future';
-    } else{
-        nineEl.classList = 'present';
-    }
-    // 10AM Time Block
-    if(10 < currentHour){
-        tenEl.classList = 'past';
-    } else if (10 > currentHour){
-        tenEl.classList = 'future';
-    } else{
-        tenEl.classList = 'present';
-    }
-    // 11AM Time Block
-    if(11 < currentHour){
-        elevenEl.classList = 'past';
-    } else if (11 > currentHour){
-        elevenEl.classList = 'future';
-    } else{
-        elevenEl.classList = 'present';
-    }
-    // 12PM Time Block
-    if(12 < currentHour){
-        twelveEl.classList = 'past';
-    } else if (12 > currentHour){
-        twelveEl.classList = 'future';
-    } else{
-        twelveEl.classList = 'present';
-    }
-    // 1PM Time Block
-    if(13 < currentHour){
-        oneEl.classList = 'past';
-    } else if (13 > currentHour){
-        oneEl.classList = 'future';
-    } else{
-        oneEl.classList = 'present';
-    }
-    // 2PM Time Block
-    if(14 < currentHour){
-        twoEl.classList = 'past';
-    } else if (14 > currentHour){
-        twoEl.classList = 'future';
-    } else{
-        twoEl.classList = 'present';
-    }
-    // 3PM Time Block
-    if(15 < currentHour){
-        threeEl.classList = 'past';
-    } else if (15 > currentHour){
-        threeEl.classList = 'future';
-    } else{
-        threeEl.classList = 'present';
-    }
-    // 4PM Time Block
-    if(16 < currentHour){
-        fourEl.classList = 'past';
-    } else if (16 > currentHour){
-        fourEl.classList = 'future';
-    } else{
-        fourEl.classList = 'present';
-    }
-    // 5PM Time Block
-    if(17 < currentHour){
-        fiveEl.classList = 'past';
-    } else if (17 > currentHour){
-        fiveEl.classList = 'future';
-    } else{
-        fiveEl.classList = 'present';
-    }
-};
+        // 9AM Time Block
+        if(9 < currentHour){
+            nineEl.classList = 'past';
+        } else if (9 > currentHour){
+            nineEl.classList = 'future';
+        } else{
+            nineEl.classList = 'present';
+        }   
+        // 10AM Time Block
+        if(10 < currentHour){
+            tenEl.classList = 'past';
+        } else if (10 > currentHour){
+            tenEl.classList = 'future';
+        } else{
+            tenEl.classList = 'present';
+        }
+        // 11AM Time Block
+        if(11 < currentHour){
+            elevenEl.classList = 'past';
+        } else if (11 > currentHour){
+            elevenEl.classList = 'future';
+        } else{
+            elevenEl.classList = 'present';
+        }
+        // 12PM Time Block
+        if(12 < currentHour){
+            twelveEl.classList = 'past';
+        } else if (12 > currentHour){
+            twelveEl.classList = 'future';
+        } else{
+            twelveEl.classList = 'present';
+        }
+        // 1PM Time Block
+        if(13 < currentHour){
+            oneEl.classList = 'past';
+        } else if (13 > currentHour){
+            oneEl.classList = 'future';
+        } else{
+            oneEl.classList = 'present';
+        }
+        // 2PM Time Block
+        if(14 < currentHour){
+            twoEl.classList = 'past';
+        } else if (14 > currentHour){
+            twoEl.classList = 'future';
+        } else{
+            twoEl.classList = 'present';
+        }
+        // 3PM Time Block
+        if(15 < currentHour){
+            threeEl.classList = 'past';
+        } else if (15 > currentHour){
+            threeEl.classList = 'future';
+        } else{
+            threeEl.classList = 'present';
+        }
+        // 4PM Time Block
+        if(16 < currentHour){
+            fourEl.classList = 'past';
+        } else if (16 > currentHour){
+            fourEl.classList = 'future';
+        } else{
+            fourEl.classList = 'present';
+        }
+        // 5PM Time Block
+        if(17 < currentHour){
+            fiveEl.classList = 'past';
+        } else if (17 > currentHour){
+            fiveEl.classList = 'future';
+        } else{
+            fiveEl.classList = 'present';
+        }
+    };
+
+var saveAgenda = function(){
+
+}
+
+buttonNineEl.addEventListener('click', () => {
+    console.log('I was clicked')
+});
+
+
 
 getCurrentTime();
